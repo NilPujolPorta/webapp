f9442ac34e9d061ab31332f0c0f5122fc68f060c
plopez8 <102725933+plopez8@users.noreply.github.com>
2023-01-16 17:00:32 +0100

    crear estructura i component login

---
+++ src/app/Projecte/Model/Entitats/Implementations/Usuari/Usuari.ts
@@ -0,0 +1,14 @@
+import { IUsuari } from "../../Interfaces/Usuari/IUsuari";
+
+export class Usuari implements IUsuari{
+    usuari: string;
+    contrasenya: string;
+    correu: string;
+
+
+    constructor(usuari: string, contrasenya: string, correu: string){
+        this.usuari = usuari;
+        this.contrasenya = contrasenya;
+        this.correu = correu;
+    }
+}
