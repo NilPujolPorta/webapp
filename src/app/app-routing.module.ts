241a0800cc2c91549ac6e79f1efd273fd8d0cd46
XavierCanal <xcanal8@boscdelacoma.cat>
2023-01-23 19:41:18 +0100

    Initial router link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalendarComponent } from './Projecte/Components/calendar/calendar.component';
+import { LoginComponent } from './Projecte/Components/login/login.component';
+import { SignupComponent } from './Projecte/Components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: SignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
