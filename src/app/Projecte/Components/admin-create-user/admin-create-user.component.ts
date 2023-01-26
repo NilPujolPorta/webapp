ece7e09e5d72569fdf5f278ad86c50dc77cc4ee1
plopez8 <102725933+plopez8@users.noreply.github.com>
2023-01-26 18:47:04 +0100

    done

---
+++ src/app/Projecte/Components/admin-create-user/admin-create-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-create-user',
+  templateUrl: './admin-create-user.component.html',
+  styleUrls: ['./admin-create-user.component.css']
+})
+export class AdminCreateUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
